533e4f8018a715a7d878d2c11d86026ef8701bc1
mkt-santagroup <marketing@santagroup.gg>
2026-02-06 03:29:31 -0300

    att

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "standalone", // <--- A Mágica da RAM
   reactCompiler: true,
+  images: {
+    // Importante para economizar CPU no servidor se você usar o componente <Image> no futuro
+    // Se estiver usando <img> normal (como está no seu código), isso não afeta tanto.
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
